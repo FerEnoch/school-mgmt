fc5004b9316e7ce6dc7a015a4ea2fbd4675366bb
Fer Enoch <ferenochsoames@gmail.com>
2024-12-09 19:31:57 -0300

    Resolution for challenge "school-mgmt"

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,26 +3,32 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
-
+    const students = await getAllStudents();
+    res.status(200).json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
-
+    const student = await addNewStudent(req.body);
+    res.status(201).json({ student });
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
     //write your code
-
+    const updatedStudent = await updateStudent(req.body);
+    res.status(200).json({ updatedStudent });
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
-
+    const student = await getStudentDetail(req.params.id);
+    res.status(200).json({ student });
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
     //write your code
+    const updatedStudentStatus = await setStudentStatus(req.body);
+    res.status(200).json({ updatedStudentStatus });
 
 });
 
